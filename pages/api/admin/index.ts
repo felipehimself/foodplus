1f0351afbb2451df12610fcefec40dd12b1ca9a1
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-08 17:08:31 -0300

    feat: included api route to delete a product

--- pages/api/admin/index.ts
+++ pages/api/admin/index.ts
@@ -1,26 +1,59 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../../lib/prismadb';
+const cloudinary = require('cloudinary').v2
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { name, price, imageId, imageUrl, category } = req.body;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+})
 
-  if (!category || !name || !price || !imageId || !imageUrl ) {
-    res.status(406).json({ message: 'Not enough data provided' });
-    return;
-  }
+interface CustomNextApiRequest extends NextApiRequest {
+  query: {
+    productId: string;
+    imageId: string;
+  };
+}
 
+export default async function handler(req: CustomNextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    
+    const { name, price, imageId, imageUrl, category } = req.body;
+
+    if (!category || !name || !price || !imageId || !imageUrl) {
+      res.status(406).json({ message: 'Not enough data provided' });
+      return;
+    }
+
     try {
-      await client.product.create({data: {name, price, imageId, imageUrl, category}})
+      await client.product.create({
+        data: { name, price, imageId, imageUrl, category },
+      });
       res.status(201).json({ message: 'created' });
     } catch (error) {
       console.log(error);
 
       res.status(400).json({ message: error });
+    }
+  }
+
+  if (req.method === 'DELETE') {
+    const { productId, imageId } = req.query;
+
+    if (!productId || !imageId) {
+      res.status(406).json({ message: 'Not enough data provided' });
+      return;
+    }
+
+    try {
+      await client.product.delete({
+        where: {
+          productId: productId,
+        },
+      });
+      await cloudinary.uploader.destroy(imageId);
+      res.status(200).json({ message: 'Product deleted' });
+    } catch (error) {
+      res.status(400).json({ message: 'Server error' });
     }
   }
 }
