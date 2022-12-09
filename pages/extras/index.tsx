824ae75641a565459a87e33a31335fb6d83f12ad
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-08 21:07:54 -0300

    feat: add page extra

--- pages/extras/index.tsx
+++ pages/extras/index.tsx
@@ -1,6 +1,30 @@
-const index = () => {
+import { GetServerSideProps } from "next";
+import ProductCard from "../../components/ProductCard";
+import ProductContainer from "../../components/ProductContainer";
+import client from "../../lib/prismadb";
+import { IProduct } from "../../types/Product";
+
+const Extras = ({ extras }: { extras: IProduct[] }) => {
   return (
-    <div>index</div>
+    <ProductContainer>
+      {extras.map((extra) => (
+        <ProductCard key={extra.id} {...extra} />
+      ))}
+    </ProductContainer>
   )
 }
-export default index
+export default Extras
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const extras = await client.product.findMany({
+    where: {
+      category: 'extra',
+    },
+  });
+
+  return {
+    props: {
+      extras,
+    },
+  };
+};
