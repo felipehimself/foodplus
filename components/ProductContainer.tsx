072be3c7de8c8ed4888f186994c8fc55bf985af8
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-09 17:15:45 -0300

    style: update gap between elements

--- components/ProductContainer.tsx
+++ components/ProductContainer.tsx
@@ -4,7 +4,7 @@ const ProductContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className='mt-6'>
       <h1 className='text-2xl font-semibold text-neutral-700 mb-8'>Customize</h1>
-      <div className='flex flex-wrap  items-center gap-5  lg:flex-row'>
+      <div className='flex flex-wrap items-center gap-8  lg:flex-row'>
         {children}
       </div>
     </section>
