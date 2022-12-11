dc7cdce16a547e6c3e3a30d9caada25f2d9bfcc6
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-11 20:12:54 -0300

    feat: implemented feature to handle page loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
 import store from '../store/store';
+import useIsPageLoading from '../hooks/useIsLoading';
+import PageLoading from '../components/PageLoading';
 
 import { Inter } from '@next/font/google';
 import Sidebar from '../components/Sidebar';
@@ -20,12 +22,12 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: ComponentWithPageLayout) {
-
   const Layout = Component.PageLayout ? Component.PageLayout : React.Fragment;
-
+  const isLoading = useIsPageLoading();
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
+        {isLoading && <PageLoading />}
         <main className={inter.className}>
           <Sidebar />
           <div className='ml-[80px] flex'>
