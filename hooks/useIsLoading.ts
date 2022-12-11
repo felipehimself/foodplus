77fdcb4f0b462bd8c16276ace8cfc39b023ad6b6
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-11 20:11:29 -0300

    feat: added hook to check if the page is loading

---
+++ hooks/useIsLoading.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+import Router from 'next/router';
+
+const useIsPageLoading = (): boolean => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => {
+      setIsLoading(true);
+    };
+    const end = () => {
+      setIsLoading(false);
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
+  return isLoading;
+};
+
+export default useIsPageLoading;
