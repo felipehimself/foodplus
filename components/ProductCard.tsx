1cd563248fa9825d4764f221dc72a6d6eefa983c
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-13 19:59:20 -0300

    update: added new argument to the function and changed className

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -7,8 +7,8 @@ import toast, { Toaster } from 'react-hot-toast';
 const ProductCard = ({ id, price, imageUrl, name, productId, category }: IProduct) => {
   const dispatch = useAppDispatch();
 
-  const handleAddToCart = (id: string, price: number, name: string, productId:string, category:string) => {
-    dispatch(addToCart({ id, price, name, productId, category }));
+  const handleAddToCart = (id: string, price: number, name: string, productId:string, category:string, imageUrl:string) => {
+    dispatch(addToCart({ id, price, name, productId, category, imageUrl }));
     toast.success('Added to cart!');
   };
 
@@ -16,11 +16,11 @@ const ProductCard = ({ id, price, imageUrl, name, productId, category }: IProduc
     <>
       <Toaster position='top-right' reverseOrder={false} />
       <button
-        onClick={() => handleAddToCart(id, price, name, productId, category)}
-        className='shadow-md hover:shadow-lg transition-all relative flex items-center justify-between w-52 h-40 bg-white rounded-md'
+        onClick={() => handleAddToCart(id, price, name, productId, category, imageUrl)}
+        className=' shadow hover:shadow-md rounded transition-all relative flex items-center justify-between w-52 h-40 bg-white '
         key={id}
       >
-        <span className='text-[10px] flex items-center justify-center rounded-full bg-neutral-700 text-white w-9 h-9 absolute -top-5 right-2'>
+        <span className='text-[10px] flex items-center justify-center  bg-neutral-600 text-white w-10 h-6 absolute -top-3 -right-2 shadow-md'>
           $ {price}
         </span>
         <div>
