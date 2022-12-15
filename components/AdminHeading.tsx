70807a938d4abcf5c79d65cb574d80c04febad58
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 14:17:32 -0300

    update: removed back button

--- components/AdminHeading.tsx
+++ components/AdminHeading.tsx
@@ -3,15 +3,16 @@ import { MdArrowBack } from 'react-icons/md';
 
 interface IProps {
   title: string;
-  width?:string;
+  width?: string;
 }
 
 const AdminHeading = ({ title, width }: IProps) => {
   return (
-    <div className={`flex mb-4 relative justify-center items-center ${width || 'lg:w-4/12'} mx-auto`}>
-      <Link href='/admin' className='absolute left-0'>
-        <MdArrowBack className='fill-slate-600' size={22} />
-      </Link>
+    <div
+      className={`flex mb-4 relative justify-center items-center ${
+        width || 'lg:w-4/12'
+      } mx-auto`}
+    >
       <h2>{title}</h2>
     </div>
   );
