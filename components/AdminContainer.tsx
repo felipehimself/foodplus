f3bff54e3573a3656d79c2d845b92e73bb832693
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 14:16:55 -0300

    style: change className

--- components/AdminContainer.tsx
+++ components/AdminContainer.tsx
@@ -2,6 +2,6 @@ interface IProps {
   children: React.ReactNode;
 }
 const AdminContainer = ({ children }: IProps) => {
-  return <div className='mt-14 mx-auto container'>{children}</div>;
+  return <div className='mt-4 mx-auto container'>{children}</div>;
 };
 export default AdminContainer;
