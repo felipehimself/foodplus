f674b958f1f301b5c5aa50842b9cde556c12ea0d
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 14:18:16 -0300

    feat: added new layout

---
+++ layouts/MainLayout.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+export default MainLayout;
