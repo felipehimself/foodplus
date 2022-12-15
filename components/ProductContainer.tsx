6988a2b74583623a8a30d1ffc42805c895801b3d
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 13:32:32 -0300

    style: changed className

--- components/ProductContainer.tsx
+++ components/ProductContainer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const ProductContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className='mt-6'>
-      <h1 className='text-2xl font-semibold text-neutral-700 mb-8'>Customize</h1>
-      <div className='flex flex-wrap items-center gap-8  lg:flex-row'>
+      <h1 className='text-xl text-center md:text-start font-semibold text-neutral-700 mb-8'>Customize</h1>
+      <div className='flex justify-center md:justify-start flex-wrap items-center gap-8'>
         {children}
       </div>
     </section>
