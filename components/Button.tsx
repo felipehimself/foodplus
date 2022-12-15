4d417a12965801d1a031f0aac994da332a3ede70
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 13:28:45 -0300

    updated: added new props

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,12 +2,16 @@ interface IProps {
   title: string;
   className?: string;
   onClick: Function;
+  type?: 'submit' | 'button';
+  disabled?:boolean
 }
-const Button = ({ title, className, onClick }: IProps) => {
+const Button = ({ title, className, onClick, type, disabled }: IProps) => {
   return (
     <button
       onClick={() => onClick()}
       className={`${className} transition-all px-7 py-3 text-xs`}
+      type={type}
+      disabled={disabled}
     >
       {title}
     </button>
