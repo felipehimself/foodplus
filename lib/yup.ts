04ec4f7b37d27ea0727ee0b1b0c6239e12d40fa1
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-15 12:12:50 -0300

    update: added new form validation

--- lib/yup.ts
+++ lib/yup.ts
@@ -4,5 +4,11 @@ export const productValidation = Yup.object().shape({
   category: Yup.string().trim().required('required'),
   name: Yup.string().trim().required('required'),
   price: Yup.number().required('required'),
- 
+});
+
+export const userDataValidation = Yup.object().shape({
+  street: Yup.string().trim().required('required'),
+  number: Yup.number(),
+  city: Yup.string().trim().required('required'),
+  state: Yup.string().trim().required('required'),
 });
