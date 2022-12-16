bfdd90335534eef53fc322719c92c0a07f0e7db8
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-16 15:13:54 -0300

    update: added features to component to either show the address or ask user to finish their register on account page

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -1,15 +1,24 @@
+import { GetServerSideProps } from 'next';
+import { unstable_getServerSession } from 'next-auth';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Button from '../../components/Button';
 import PageLoading from '../../components/PageLoading';
 import MainLayout from '../../layouts/MainLayout';
+import client from '../../lib/prismadb';
 import { RootState } from '../../store/store';
+import { IAddress } from '../../types/User';
+import { authOptions } from '../api/auth/[...nextauth]';
 
-const Checkout = () => {
+const Checkout = ({ userAddress }: { userAddress: IAddress }) => {
+  const [paymentMethod, setPaymentMethod] = useState('');
+
   const { order, totalAmt } = useSelector((state: RootState) => state.cart);
+
   // const { data: session, status  } = useSession();
 
   // const router = useRouter();
@@ -26,73 +35,88 @@ const Checkout = () => {
         <div className=' w-full sm:w-5/12 md:w-4/12 p1 rounded-md bg-white shadow-md'>
           <div className='p-3 space-y-4'>
             <div>
-              <h3 className='font-semibold'>Address</h3>
+              <h2 className='font-semibold text-lg'>Address</h2>
               <div>
-                <label htmlFor='paymentMethod'>Pay on delivery</label>
-                <input
-                  type='radio'
-                  id='paymentMethod'
-                  className='accent-primary-500'
-                />
+                {!userAddress ? (
+                  <p>
+                    <Link className='text-accent-400 underline' href='/account'>
+                      Register an address
+                    </Link>{' '}
+                    to continue
+                  </p>
+                ) : (
+                  <>
+                    <p>
+                      {userAddress?.street}, {userAddress?.number}
+                    </p>
+                    <p>
+                      {userAddress.city} - {userAddress.state}
+                    </p>
+                  </>
+                )}
               </div>
             </div>
             <div className='space-y-2'>
-              <h3 className='font-semibold'>Payment Method</h3>
+              <h2 className='font-semibold'>Payment Method</h2>
               <div className='flex gap-2 items-center'>
-                <label htmlFor='paymentMethod'>On Delivery</label>
+                <label htmlFor='paymentMethod' className='text-sm'>
+                  On Delivery
+                </label>
                 <input
                   type='radio'
                   id='paymentMethod'
                   className='accent-primary-500'
+                  value='on delivery'
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                  disabled={order.length === 0}
                 />
               </div>
-              {/* <div className='flex gap-2 items-center'>
-                <label htmlFor="paymentMethod">Cash</label>
-                <input type='radio' id='paymentMethod' className='accent-primary-500' />
-              </div>
-              <div className='flex gap-2 items-center'>
-                <label htmlFor="paymentMethod">Credit Card</label>
-                <input type='radio' id='paymentMethod' className='accent-primary-500' />
-              </div> */}
             </div>
           </div>
         </div>
         <div className=' w-full sm:w-5/12 md:w-4/12 rounded-md bg-white shadow-md p-1'>
           <div className='p-3 space-y-2'>
             <div>
-              <h3 className='text-center font-semibold mb-1 '>Checkout</h3>
+              <h2 className='text-center mb-1 '>Checkout</h2>
             </div>
-            {order.map((product) => (
-              <div className='flex gap-2 items-end' key={product.id}>
-                <div className='-mb-2'>
-                  <Image
-                    src={product.imageUrl}
-                    width={60}
-                    height={60}
-                    alt={product.name}
+            {order.length > 0 ? (
+              <>
+                {order.map((product) => (
+                  <div className='flex gap-2 items-end' key={product.id}>
+                    <div className='-mb-2'>
+                      <Image
+                        src={product.imageUrl}
+                        width={60}
+                        height={60}
+                        alt={product.name}
+                      />
+                    </div>
+                    <div className='flex text-sm  items-end'>
+                      <div>
+                        <p>{product.name}</p>
+                        <p>$ {product.price.toFixed(2)}</p>
+                      </div>
+                    </div>
+                    <span className='text-sm flex-1 text-right'>
+                      x {product.qty}
+                    </span>
+                  </div>
+                ))}
+                <div className='flex font-semibold justify-between pt-3'>
+                  <span>Total</span> <span>$ {totalAmt.toFixed(2)}</span>
+                </div>
+                <div className='pt-2'>
+                  <Button
+                    onClick={() => {}}
+                    className='bg-primary-500 rounded hover:bg-primary-600 w-full text-white disabled:bg-slate-400'
+                    title='SEND'
+                    disabled={!paymentMethod || !paymentMethod}
                   />
                 </div>
-                <div className='flex text-sm  items-end'>
-                  <div>
-                    <p>{product.name}</p>
-                    <p>$ {product.price.toFixed(2)}</p>
-                  </div>
-                </div>
-                <span className='text-sm flex-1 text-right'>
-                  x {product.qty}
-                </span>
-              </div>
-            ))}
-            <div className='flex font-semibold justify-between pt-3'>
-              <span>Total</span> <span>$ {totalAmt.toFixed(2)}</span>
-            </div>
-            <div className='pt-2'>
-              <Button
-                onClick={() => {}}
-                className='bg-primary-500 rounded hover:bg-primary-600 w-full text-white'
-                title='SEND'
-              />
-            </div>
+              </>
+            ) : (
+              <h3 className='text-center'>You have no items in your cart</h3>
+            )}
           </div>
         </div>
       </section>
@@ -100,7 +124,33 @@ const Checkout = () => {
   );
 };
 
-Checkout.PageLayout = MainLayout
+Checkout.PageLayout = MainLayout;
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await unstable_getServerSession(
+    ctx.req,
+    ctx.res,
+    authOptions
+  );
+
+  if (!session?.user.email) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/sauces',
+      },
+    };
+  }
+
+  const userAddress = await client.address.findUnique({
+    where: {
+      userId: session?.user.email,
+    },
+  });
+
+  return {
+    props: { userAddress },
+  };
+};
 
 export default Checkout;
