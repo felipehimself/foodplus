89668fec5f2632eb45334bcd387d628de32c1a17
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-16 15:07:35 -0300

    update: added separated variables for hot toast effects

---
+++ lib/hot-toast.ts
@@ -0,0 +1,17 @@
+export const savingAddress = {
+  loading: 'Saving...',
+  success: 'Address saved',
+  error: 'Something went wrong',
+};
+
+export const deletingProduct = {
+  loading: 'Deleting...',
+  success: 'Product deleted',
+  error: 'Something went wrong',
+};
+
+export const savingProduct = {
+  loading: 'Saving...',
+  success: 'Product saved',
+  error: 'Something went wrong',
+};
