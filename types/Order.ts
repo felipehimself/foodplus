25904d9528a1a8ade9fa9cf925fc508e9694cd99
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-17 16:17:55 -0300

    update: changed property name and included new interface

--- types/Order.ts
+++ types/Order.ts
@@ -1,6 +1,17 @@
 import { IProduct } from './Product';
 
 export interface IOrder extends IProduct {
-  qty: number;
+  quantity: number;
+}
+
+export interface IUserOrder extends IOrder {
+  userId:string	
+  orderId:string
+  productId:string	
+  paymentMethod: string
+}
+
+export interface IUserOrders extends IUserOrder {
+  createdAt: Date
 }
 
