106865bfb7069489eef56847d7d535b9220c9725
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-19 16:29:14 -0300

    update: added font subset and removed dynamic title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-const inter = Inter();
+const inter = Inter({ subsets: ['latin'] });
 
 type ComponentWithPageLayout = AppProps & {
   Component: AppProps['Component'] & {
@@ -25,27 +25,11 @@ export default function App({
   const Layout = Component.PageLayout ? Component.PageLayout : React.Fragment;
   const isLoading = useIsPageLoading();
 
-  const { pathname } = useRouter();
-
-  const getPageTitle = () : string => {
-    const title = pathname.split('/')[1];
-    const [first, ...rest] = title.split('');
-
-    return first.toUpperCase() + rest.join('');
-  };
-
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
         {isLoading && <PageLoading />}
         <main className={inter.className}>
-          <Head>
-            <title>{getPageTitle()}</title>
-            <meta
-              name='viewport'
-              content='initial-scale=1.0, width=device-width'
-            />
-          </Head>
           <Layout>
             <Component {...pageProps} />
           </Layout>
