43ea8add521b0e2dd9dc030161dccc302440a030
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-19 13:45:16 -0300

    refactor: removed unnecessary logic

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -29,20 +29,13 @@ const callbackOptions = {
 }
 
 const SignIn = ({ providers }: any) => {
-  const { data: session, status  } = useSession();
 
   const { query: { callback } } = useRouter();
 
-  const router = useRouter();
 
-  // useEffect(() => {
-  //   if ((status !== 'loading') && session) {
-  //     router.back();
-  //   }
-  // }, [session, router, status]);
 
   return (
-    <div className='mt-4 bg-white flex flex-col gap-6 justify-center items-center'>
+    <div className='mt-12 bg-white flex flex-col gap-6 justify-center items-center'>
       <div className='rounded border p-6 space-y-3'>
         <h1 className='text-lg text-center'>Sign in to continue</h1>
         {Object.values(providers).map((provider: any) => (
@@ -56,7 +49,7 @@ const SignIn = ({ providers }: any) => {
             <button
               className='flex items-center gap-4'
               onClick={() => signIn(provider.id, {callbackUrl:callbackOptions[callback as keyof typeof callbackOptions] || '/sauces'})}
-              disabled={status == 'loading'}
+              
             >
               {icons[provider.id as keyof typeof icons]['icon']}
               Sign in with {provider.name}
