c00d9ce31969a96696783196748e783669f297c4
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-19 12:59:02 -0300

    update: updated interface declaration

--- types/Order.ts
+++ types/Order.ts
@@ -5,13 +5,25 @@ export interface IOrder extends IProduct {
 }
 
 export interface IUserOrder extends IOrder {
-  userId:string	
-  orderId:string
-  productId:string	
-  paymentMethod: string
+  userId: string;
+  orderId: string;
+  productId: string;
+  paymentMethod: string;
+  createdAt: Date;
 }
 
-export interface IUserOrders extends IUserOrder {
-  createdAt: Date
+export interface IItems {
+  quantity: number;
+  name: string;
+  price: number;
+  totalAmt: number;
+  productId: string;
+  imageUrl: string;
 }
 
+export interface IUserOrders {
+  orderId: string;
+  createdAt: Date;
+  paymentMethod: string;
+  items: IItems[];
+}
