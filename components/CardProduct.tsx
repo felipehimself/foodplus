5f0a918b4664199176acc10330de5ce8f96226a5
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-22 13:48:17 -0300

    style: changed style to show different element on small screens

--- components/CardProduct.tsx
+++ components/CardProduct.tsx
@@ -31,16 +31,19 @@ const CardProduct = ({
         onClick={() =>
           handleAddToCart(price, name, productId, category, imageUrl)
         }
-        className='p-2 rounded-md bg-white shadow-md hover:shadow-lg transition-all relative flex items-center justify-between w-52 h-40'
+        className='p-4 rounded-md bg-white shadow-md hover:shadow-lg transition-all relative flex items-center justify-between w-52 h-40'
         key={id}
       >
-        <span className='text-2xs rounded-sm flex items-center justify-center  bg-accent-400 text-white w-11 h-6 absolute -top-3 -right-2 shadow-md'>
+        <span className='text-2xs rounded-sm hidden md:flex items-center justify-center  bg-accent-400 text-white w-11 h-6 absolute -top-3 -right-2 shadow-md'>
           $ {price.toFixed(2)}
         </span>
         <div>
           <Image src={imageUrl} alt={name} width={100} height={100} />
         </div>
-        <p className='flex-1 text-lg text-slate-700'>{name}</p>
+        <div>
+          <p className='flex-1 text-lg text-slate-700'>{name}</p>
+          <p className='md:hidden text-accent-400 text-sm font-semibold'>$ {price.toFixed(2)}</p>
+        </div>
       </button>
     </>
   );
