f831731eb77923d63dd5550d5ae09b1b982d7ca9
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-21 21:19:38 -0300

    update: changed layout to be adaptive on small screens and medium

--- layouts/CartLayout.tsx
+++ layouts/CartLayout.tsx
@@ -2,28 +2,27 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Cart from '../components/Cart';
 import { RootState } from '../store/store';
-import { useAppDispatch } from '../store/store';
-import { toggleShowCart } from '../features/showCartSlice';
 import Sidebar from '../components/Sidebar';
+import MobileTab from '../components/MobileTab';
+import MobileNavbar from '../components/MobileNavbar';
+import MobileCart from '../components/MobileCart';
 
 export const CartLayout = ({ children }: { children: React.ReactNode }) => {
-  const dispatch = useAppDispatch();
+  
   const { showCart } = useSelector((state: RootState) => state.showCart);
 
-  const handleShowCart = () => {
-    dispatch(toggleShowCart(false));
-  };
-
   return (
-    <div style={{marginLeft:'5rem'}} className='ml-[4.75rem] md:ml-[5rem] flex'>
-      <Sidebar />
-      <Cart showCart={showCart} />
-      <div
-        onClick={handleShowCart}
-        className='w-full h-screen overflow-y-auto py-2 px-6'
-      >
-        {children}
+    <>
+      <MobileNavbar />
+      <div className='flex'>
+        <Sidebar />
+        <MobileTab />
+        <MobileCart showCart={showCart} />
+        <Cart />
+        <div className='w-full h-screen overflow-y-auto py-2 px-6'>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
