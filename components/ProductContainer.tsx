c4bc1cecafb795b7851d28f3e969f5210c7be2be
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-24 17:19:33 -0300

    update: added logic to show the tag only for specific routes

--- components/ProductContainer.tsx
+++ components/ProductContainer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const ProductContainer = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
   return (
     <section className='mt-6'>
-      <h1 className='text-xl text-center md:text-start font-semibold text-neutral-700 mb-8'>Customize</h1>
+      {router.pathname !== '/' && (
+        <h1 className='text-xl text-center md:text-start font-semibold text-neutral-700 mb-8'>
+          Customize
+        </h1>
+      )}
       <div className='flex justify-center md:justify-start flex-wrap items-center gap-8'>
         {children}
       </div>
