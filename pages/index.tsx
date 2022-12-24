5871bf7f194fa605f0d92aa326b28f32dcc74907
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-24 17:20:18 -0300

    feat: added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-// index.tsx
-export default function Page() {
-  const { data: session } = useSession()
+import { CartLayout } from '../layouts/CartLayout';
+import welcome from './../public/welcome-home-webp.webp';
 
+import Image from 'next/image';
 
-
-  if (session) {
-    return (
-      <>
-        Signed in as {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const Home = () => {
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
+    <div className='mx-auto md:mt-4 flex flex-col gap-5 justify-center items-center'>
+      <h2 className='text-2xl'>Welcome to FoodPlus</h2>
+      <div>
+        <Image src={welcome} width={400} height={400} alt='Welcome image' />
+      </div>
+      <p className='text-xl text-center'>
+        Start adding items to your cart <br /> by navigating through the menu{' '}
+        <span className='hidden md:inline'>on the left</span>
+        <span className='md:hidden'>below</span>
+      </p>
+    </div>
+  );
+};
+
+Home.PageLayout = CartLayout;
+
+export default Home;
