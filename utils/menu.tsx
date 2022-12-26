bf03ab2f660839bb56df32799b92b4ab1a9b9510
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-26 13:45:50 -0300

    update: added new variables to file

--- utils/menu.tsx
+++ utils/menu.tsx
@@ -1,3 +1,4 @@
+import { BsGithub } from 'react-icons/bs';
 import {
   FaCarrot,
   FaCheese,
@@ -5,11 +6,50 @@ import {
   FaWineBottle,
   FaAppleAlt,
 } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 export const menuItems = [
-  { item: 'sauces', icon: <FaWineBottle color='#fff' size={18} />, path: 'sauces' },
-  { item: 'veggies', icon: <FaCarrot color='#fff' size={18} />, path: 'veggies' },
+  {
+    item: 'sauces',
+    icon: <FaWineBottle color='#fff' size={18} />,
+    path: 'sauces',
+  },
+  {
+    item: 'veggies',
+    icon: <FaCarrot color='#fff' size={18} />,
+    path: 'veggies',
+  },
   { item: 'cheese', icon: <FaCheese color='#fff' size={15} />, path: 'cheese' },
-  { item: 'crunch', icon: <FaSeedling color='#fff' size={18} />, path: 'crunch' },
-  { item: 'extras', icon: <FaAppleAlt color='#fff' size={18} />, path: 'extras' },
+  {
+    item: 'crunch',
+    icon: <FaSeedling color='#fff' size={18} />,
+    path: 'crunch',
+  },
+  {
+    item: 'extras',
+    icon: <FaAppleAlt color='#fff' size={18} />,
+    path: 'extras',
+  },
 ];
+
+export const signinIcons = {
+  google: {
+    icon: <FcGoogle size={20} />,
+    bgColor: 'bg-white',
+    txtColor: 'text-neutral-600',
+  },
+  github: {
+    icon: <BsGithub size={20} />,
+    bgColor: 'bg-black',
+    txtColor: 'text-white',
+  },
+};
+
+export const callbackOptions = {
+  sauces: '/sauces',
+  veggies: '/veggies',
+  cheese: '/cheese',
+  crunch: '/crunch',
+  extras: '/extras',
+  checkout: '/checkout',
+};
