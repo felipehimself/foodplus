bb94874e521e1829f411f415f1e0c6e1dfa54068
Felipe Couto <felipecoutodarocha@gmail.com>
2022-12-27 14:49:15 -0300

    style: changed element position to be centered on the screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,18 @@ import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div className='mx-auto pt-5 md:h-screen md:pt-0 flex flex-col gap-5 justify-center items-center'>
-      <h2 className='text-2xl'>Welcome to FoodPlus</h2>
-      <div>
-        <Image src={welcome} width={400} height={400} alt='Welcome image' />
+    <div className='absolute w-10/12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className="flex flex-col items-center justify-center gap-4 md:ml-[5rem]">
+        <h2 className='text-2xl md:text-3xl text-center'>Welcome to FoodPlus</h2>
+        <div>
+          <Image src={welcome} width={400} height={400} alt='Welcome image' />
+        </div>
+        <p className='text-lg md:text-xl text-center'>
+          Start adding items to your cart <br /> by navigating through the menu{' '}
+          <span className='hidden md:inline'>on the left</span>
+          <span className='md:hidden'>below</span>
+        </p>
       </div>
-      <p className='text-xl text-center'>
-        Start adding items to your cart <br /> by navigating through the menu{' '}
-        <span className='hidden md:inline'>on the left</span>
-        <span className='md:hidden'>below</span>
-      </p>
     </div>
   );
 };
